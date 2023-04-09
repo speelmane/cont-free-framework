6a46ea1e0207137734e1be24f75f2329b3f9910d
Everita <EveritaAnnemarija.Samusa@verifone.com>
2023-04-09 23:31:35 +0300

    Discarded header files for a __less places to manage__  approach

--- src/task1.c
+++ src/task1.c
@@ -1,7 +1,29 @@
 #include "common.h" // subschedule definition and 
-#include "task1.h" //data type, attribute and section start/end definitions (TODO)
 #include <string.h>
 #include <stdio.h>
+
+/**
+ * Create input and output typedefs, put all necessary values WITHIN the structs only.
+**/
+typedef struct 
+{
+    uint32_t coefficients[8];
+} data_in_t;
+
+typedef struct 
+{
+    uint32_t coefficients[8];
+} data_out_t;
+
+/**
+ * Local data type definition, do not modify.
+ * */
+typedef struct 
+{
+    data_in_t local_data_in;
+    data_out_t local_data_out;
+} local_data_t;
+
 
 #define __task1_runtime_copy(group) __attribute__((used, section(".task1_runtime_copy." group)))
 
